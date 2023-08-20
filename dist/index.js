c0ca65eb83ba3de6e55b4c28defb4348ee0b7ed4
francis <franzdiaz460@gmail.com>
2023-08-21 00:03:25 +0400

    ref: extracting and prompting the choices of keys from events choices

--- dist/index.js
+++ dist/index.js
@@ -2,24 +2,31 @@
 import EventEmitter from "events";
 import fetch from "node-fetch";
 import Enquirer from "enquirer";
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
-// const warframeEvents = require("./app-queries/events.json");
-// const languages = require("./app-queries/languages.json")
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const warframeEvents = require("./app-queries/events.json");
+const languages = require("./app-queries/languages.json");
 const event = new EventEmitter();
 const warframeAPI = `https://api.warframestat.us/pc/`; //path parameters+language
-console.log("Init wf cli");
-async function getUserInput() {
-    const getInputs = await Enquirer.prompt({
+event.addListener("start", getUserInput);
+event.emit("start", warframeEvents);
+async function getUserInput(wfEvents) {
+    console.log("Init wf cli");
+    const getInputs = (await Enquirer.prompt({
         type: "select",
         name: "eventType",
         message: "Pick an event",
-        choices: ["ge", "lo", "fa"],
-    });
-    console.log(getInputs);
-    return { eventType: "this is a string", languageType: "en" };
+        choices: wfEvents.map((item) => {
+            const keys = Object.keys(item);
+            return keys[0];
+        }),
+    }));
+    console.log({ e: getInputs.eventType });
+    return {
+        eventType: getInputs.eventType,
+        languageType: getInputs.languageType,
+    };
 }
-getUserInput();
 async function fetchEventData(api, { eventType, languageType }) {
     try {
         const getData = await fetch(`${api}${eventType}?language=${languageType}`);
--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,61 @@
 import EventEmitter from "events";
 import fetch from "node-fetch";
 import Enquirer from "enquirer";
-// import { createRequire } from "module";
+import { createRequire } from "module";
+import { SrvRecord } from "dns";
+import { get } from "http";
 
-// const require = createRequire(import.meta.url);
-// const warframeEvents = require("./app-queries/events.json");
-// const languages = require("./app-queries/languages.json")
+const require = createRequire(import.meta.url);
+const warframeEvents = require("./app-queries/events.json");
+const languages = require("./app-queries/languages.json");
 const event = new EventEmitter();
 const warframeAPI = `https://api.warframestat.us/pc/`; //path parameters+language
 interface IWarframeEventStatus {
   [key: string]: any;
+}
+
+interface IWarframeEvents {
+  [event: string]: string;
+  // "cetus (eidolon-hunts)": string;
+  // archon: string;
+  // sortie: string;
+  // "cambion-drift": string;
+  // nightwave: string;
+  // "Void trader": string;
+  // "Vault trader": string;
+  // "Orb valis": string;
+  // Fissures: string;
 }
 interface IUserInput {
   eventType: string;
   languageType: string;
 }
 
-console.log("Init wf cli");
+event.addListener("start", getUserInput);
+event.emit("start", warframeEvents);
 
-async function getUserInput(): Promise<IUserInput> {
-  const getInputs = await Enquirer.prompt({
+async function getUserInput(
+  wfEvents: Array<{ [event: string]: string }>
+): Promise<IUserInput> {
+  console.log("Init wf cli");
+  const getEventInput = (await Enquirer.prompt({
     type: "select",
     name: "eventType",
     message: "Pick an event",
-    choices: ["ge", "lo", "fa"],
-  });
-  console.log(getInputs);
-  return { eventType: "this is a string", languageType: "en" };
+    choices: wfEvents.map((item) => {
+      const keys = Object.keys(item);
+      return keys[0];
+    }),
+  })) as IUserInput;
+
+  console.log({ e: getEventInput.eventType });
+  return {
+    eventType: getEventInput.eventType,
+    languageType: getEventInput.languageType,
+  };
 }
 
-getUserInput();
+async function evaluateUserInput(input: IUserInput) {}
 
 async function fetchEventData(
   api: string,
