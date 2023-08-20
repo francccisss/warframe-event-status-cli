a599f9c738dfe5c4e035d3c4e5fbf45d737da085
francis <franzdiaz460@gmail.com>
2023-08-20 21:31:27 +0400

    ref: abel to fetch warframe event status

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,26 @@
 #! /usr/bin/env node
 import EventEmitter from "events";
+import fetch from "node-fetch";
 const event = new EventEmitter();
-const num = 32;
+const warframeAPI = `https://api.warframestat.us/pc/`; //path parameters+language
 console.log("Init wf cli");
+async function getUserInput() {
+    return { missionType: "this is a string", languageType: "en" };
+}
+async function fetchEventData(api, { missionType, languageType }) {
+    try {
+        const getData = await fetch(`${api}${missionType}?language=${languageType}`);
+        const parseToJson = await getData.json();
+        console.log(parseToJson);
+        return parseToJson;
+    }
+    catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+const data = await fetchEventData(warframeAPI, {
+    missionType: "sortie",
+    languageType: "en",
+});
+console.log(data);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 #! /usr/bin/env node
 import EventEmitter from "events";
-
+import fetch from "node-fetch";
 interface IWarframeEventStatus {
   [key: string]: any;
 }
+interface IUserInput {
+  missionType: string;
+  languageType: string;
+}
+
 const event = new EventEmitter();
 const warframeAPI = `https://api.warframestat.us/pc/`; //path parameters+language
 console.log("Init wf cli");
 
-async function fetchEventData(api: string): Promise<IWarframeEventStatus | {}> {
-  return {};
+async function getUserInput(): Promise<IUserInput> {
+  return { missionType: "this is a string", languageType: "en" };
 }
+
+async function fetchEventData(
+  api: string,
+  { missionType, languageType }: IUserInput
+): Promise<IWarframeEventStatus | null> {
+  try {
+    const getData = await fetch(
+      `${api}${missionType}?language=${languageType}`
+    );
+    const parseToJson = await getData.json();
+    console.log(parseToJson);
+    return parseToJson as IWarframeEventStatus;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+const data = await fetchEventData(warframeAPI, {
+  missionType: "sortie",
+  languageType: "en",
+});
+console.log(data);
