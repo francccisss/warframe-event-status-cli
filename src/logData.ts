bd64a9a9e5d88145218ee9918e28d3afd20498e1
francis <franzdiaz460@gmail.com>
2023-08-21 22:24:59 +0400

    ref: separated data extractor function to reduce clutter and

--- src/logData.ts
+++ src/logData.ts
@@ -6,26 +6,15 @@ async function logCurrentEventStatus(
   data: IWarframeEventStatus,
   wfEvents: IWarframeEvents
 ): Promise<void> {
-  const flatVertTable = createFlatTable(data, "vert", "state", "timeLeft");
+  const flatVertTable = createFlatTable(data, "hor");
   console.log(flatVertTable);
 }
 
 function createFlatTable(
   data: IWarframeEventStatus,
-  type: "vert" | "hor" = "vert",
-  ...args: any[]
+  type: "vert" | "hor" = "vert"
 ): string {
-  let reducedData = {};
-  const getKeys = Object.keys(data as object);
-  if (args.length !== 0) {
-    for (let i = 0; i < args.length; i++) {
-      for (let j = 0; j < getKeys.length; j++) {
-        if (args[i] === getKeys[j]) {
-          reducedData = { [args[i]]: data[args[i]], ...reducedData };
-        }
-      }
-    }
-  }
+  const reducedData = extractRelevantData(data);
   const reducedDataKeys = Object.keys(reducedData);
   if (type === "hor") {
     const createColWidths = reducedDataKeys.map(() => {
@@ -49,53 +38,36 @@ function createFlatTable(
     return "";
   }
 }
+
+function extractRelevantData(data: IWarframeEventStatus): IWarframeEventStatus {
+  const relevantData: Array<string> = [
+    "state",
+    "timeLeft",
+    "location",
+    "activation",
+    "active",
+    "expiry",
+    "inventory",
+  ];
+  let reducedData = {};
+  const getKeys = Object.keys(data as object);
+  if (relevantData.length !== 0) {
+    for (let i = 0; i < relevantData.length; i++) {
+      for (let j = 0; j < getKeys.length; j++) {
+        if (relevantData[i] === getKeys[j]) {
+          reducedData = {
+            [relevantData[i]]: data[relevantData[i]],
+            ...reducedData,
+          };
+        }
+      }
+    }
+  }
+
+  return reducedData;
+}
+
 export default logCurrentEventStatus;
-
-// what to log from events
-// {
-//   isDay: true,
-//   state: 'day',
-//   isCetus: true,
-//   shortString: '1h 17m to Night'
-// }
-// ______________________________
-// | State  |       Time        |
-// ------------------------------
-// |  Day   |  1h 17mn to Night |
-// ------------------------------
-
-// BARO
-// {
-//   activation: '2023-08-25T13:00:00.000Z',
-//   startString: '4d 10h 51m 1s',
-//   expiry: '2023-08-27T13:00:00.000Z',
-//   active: false,
-//   character: "Baro Ki'Teer",
-//   location: 'Strata Relay (Earth)',
-//   inventory: [],
-//   endString: '6d 10h 51m 1s',
-//   schedule: []
-// }
-
-// FISSURES grouped within an array of the same relic type
-// {
-//     activation: '2023-08-21T02:20:01.834Z',
-//     startString: '-1m 56s',
-//     expiry: '2023-08-21T03:50:01.834Z',
-//     active: true,
-//     node: 'Lu-yan (Veil)',
-//     missionType: 'Survival',
-//     missionKey: 'Survival',
-//     enemy: 'Corpus',
-//     enemyKey: 'Corpus',
-//     nodeKey: 'Lu-yan (Veil)',
-//     tier: 'Axi',
-//     tierNum: 4,
-//     expired: false,
-//     eta: '1h 28m 3s',
-//     isStorm: true,
-//     isHard: false
-// }
 
 // ARCHON
 
@@ -191,59 +163,7 @@ export default logCurrentEventStatus;
 //       ducats: null,
 //       credits: 1
 //     }
-//   ],
-//   endString: '17d 15h 32m 1s',
-//   initialStart: '2022-09-09T15:42:24.266Z',
-//   completed: false,
-//   schedule: [
-//     {
-//       expiry: '2022-11-03T18:00:00.000Z',
-//       item: 'M P V Equinox Wukong Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2022-12-01T19:00:00.000Z',
-//       item: 'M P V Valkyr Saryn Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-01-05T19:00:00.000Z',
-//       item: 'M P V Oberon Prime Single Pack'
-//     },
-//     {
-//       expiry: '2023-02-02T19:00:00.000Z',
-//       item: 'M P V Volt Loki Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-03-02T19:00:00.000Z',
-//       item: 'M P V Atlas Vauban Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-04-06T18:00:00.000Z',
-//       item: 'M P V Nekros Oberon Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-05-04T18:00:00.000Z',
-//       item: 'M P V Mag Rhino Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-06-02T15:00:00.000Z',
-//       item: 'M P V Nekros Oberon Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-07-06T18:00:00.000Z',
-//       item: 'M P V Inaros Ash Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-08-03T18:00:00.000Z',
-//       item: 'M P V Banshee Mirage Prime Dual Pack'
-//     },
-//     {
-//       expiry: '2023-09-07T18:00:00.000Z',
-//       item: 'M P V Rhino Nyx Prime Dual Pack'
-//     },
-//     { expiry: '2023-10-05T18:00:00.000Z' },
-//     { expiry: '2023-11-02T18:00:00.000Z' }
-//   ]
-// }
+//   ]}
 
 // ORB VALLIS
 // {
