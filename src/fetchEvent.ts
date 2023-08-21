54b7a2cfad4eded5aca8d7ff4ac9c6b6a39bad7a
francis <franzdiaz460@gmail.com>
2023-08-21 04:43:10 +0400

    ref: add boxen library for welcome log

--- src/fetchEvent.ts
+++ src/fetchEvent.ts
@@ -1,16 +1,23 @@
 import fetch from "node-fetch";
 import { IWarframeEventStatus, IUserInput } from "./types.js";
+import { createSpinner } from "nanospinner";
 async function fetchEventData(
   api: string,
   { eventType, language }: IUserInput
 ): Promise<IWarframeEventStatus | null> {
+  const spinner = createSpinner("Fetching current event status...").start();
   try {
     const getData = await fetch(`${api}${eventType}?language=${language}`);
     const parseToJson = await getData.json();
+    spinner.success({ text: "Current status", mark: "✔" });
     console.log(parseToJson);
     return parseToJson as IWarframeEventStatus;
   } catch (err) {
     console.log(err);
+    spinner.success({
+      text: "Sorry, but we are not able to get the current status of the chosen event",
+      mark: "☠",
+    });
     return null;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import EventEmitter from "events";
 import { createRequire } from "module";
 import fetchEventData from "./fetchEvent.js";
 import { getUserInput } from "./inputs.js";
+import boxen from "boxen";
 
 const require = createRequire(import.meta.url);
 const warframeEvents = require("./app-queries/events.json");
@@ -11,8 +12,18 @@ const event = new EventEmitter();
 const warframeAPI = `https://api.warframestat.us/pc/`; //path parameters+language
 
 event.on("start", async (warframeEvents, queryLanguages) => {
+  console.log(
+    boxen("Warframe Event Status CLI", {
+      title: "Fdiaz",
+      titleAlignment: "center",
+      padding: { top: 4, bottom: 4, left: 2, right: 2 },
+      borderStyle: "round",
+    })
+  );
   const input = await getUserInput(warframeEvents, queryLanguages);
   console.log(input);
-  event.emit("fetch", input);
+  event.emit("fetch", warframeAPI, input);
 });
 event.emit("start", warframeEvents, queryLanguages);
+
+event.on("fetch", fetchEventData);
