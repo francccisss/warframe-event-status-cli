522141f20ce745ee949d41aef2a906445a68c0ca
francis <franzdiaz460@gmail.com>
2023-08-23 06:32:55 +0400

    ref: able to extract and log extracted data and making it more reusable

--- src/logData.ts
+++ src/logData.ts
@@ -10,6 +10,7 @@ async function logCurrentEventStatus(
   data: IWarframeEventStatus,
   wfEvents: IWarframeEvents
 ): Promise<void> {
+  console.log(data);
   const { flatTable, nested } = createFlatTable(data, "hor");
   if (nested) {
     console.log(flatTable);
@@ -23,70 +24,6 @@ function createFlatTable(
   data: IWarframeEventStatus,
   type: "vert" | "hor" = "vert"
 ): { flatTable: string; nested?: string } {
-  const { reducedData, nestedData } = extractRelevantData(data);
-  const reducedDataKeys = Object.keys(reducedData).map((name) =>
-    name.toUpperCase()
-  );
-  if (type === "hor") {
-    const createColWidths = reducedDataKeys.map(() => {
-      return 20;
-    });
-    var table = new Table({
-      head: reducedDataKeys,
-      colWidths: createColWidths,
-    });
-    const newTable = Object.values(reducedData as object);
-    table.push(newTable);
-    if (nestedData !== null) {
-      const nested = createNestedTable(nestedData);
-      return { flatTable: table.toString(), nested };
-    }
-    return { flatTable: table.toString() };
-  } else if (type === "vert") {
-    var table = new Table();
-    for (const [key, value] of Object.entries(reducedData as object)) {
-      const newTable = { [key]: value };
-      table.push(newTable);
-    }
-    if (nestedData !== null) {
-      console.log("has nested data");
-      const nested = createNestedTable(nestedData);
-      return { flatTable: table.toString(), nested };
-    }
-    return { flatTable: table.toString() };
-  } else {
-    return { flatTable: "", nested: "" };
-  }
-}
-
-function createNestedTable(
-  nestedTableArray: Array<IWarframeEventStatus>
-): string {
-  const nestedKey = Object.keys(nestedTableArray)[0];
-  const nestedArray = nestedTableArray[nestedKey as any];
-  const table = new Table({
-    head: [
-      "",
-      nestedKey.toUpperCase(),
-      ...(nestedArray.length !== 0
-        ? Object.keys(nestedArray[0]).map((name) => name.toUpperCase())
-        : []),
-    ],
-  });
-
-  nestedArray.forEach((value: object, i: number) => {
-    let tmpArr = [];
-    for (let j = 0; j < Object.values(value).length; j++) {
-      Object.values(value)[j] !== null
-        ? tmpArr.push(Object.values(value)[j].toString())
-        : tmpArr.push("missing data");
-    }
-    table.push({ [i]: tmpArr });
-  });
-  return table.toString();
-}
-
-function extractRelevantData(data: IWarframeEventStatus): IWarframeEventStatus {
   const relevantData: Array<string> = [
     "state",
     "timeLeft",
@@ -106,20 +43,117 @@ function extractRelevantData(data: IWarframeEventStatus): IWarframeEventStatus {
     "eta",
     "missionType",
   ];
+  const { reducedData, nestedData } = extractRelevantData(data, relevantData);
+  const reducedDataKeys = Object.keys(reducedData).map((name) =>
+    name.toUpperCase()
+  );
+  if (type === "hor") {
+    const createColWidths = reducedDataKeys.map(() => {
+      return 20;
+    });
+    var table = new Table({
+      head: reducedDataKeys,
+      colWidths: createColWidths,
+    });
+    const newTable = Object.values(reducedData as object);
+    table.push(newTable);
+    if (nestedData !== null) {
+      // const nested = createNestedTable(nestedData);
+      const nested = "";
+      return { flatTable: table.toString(), nested };
+    }
+    return { flatTable: table.toString() };
+  } else if (type === "vert") {
+    var table = new Table();
+    for (const [key, value] of Object.entries(reducedData as object)) {
+      const newTable = { [key]: value };
+      table.push(newTable);
+    }
+    if (nestedData !== null) {
+      console.log("has nested data");
+      // const nested = createNestedTable(nestedData);
+      const nested = "";
+      return { flatTable: table.toString(), nested };
+    }
+    return { flatTable: table.toString() };
+  } else {
+    return { flatTable: "", nested: "" };
+  }
+}
+
+function createNestedTable(
+  nestedTableArray: Array<IWarframeEventStatus>
+): string {
+  const relevantData: Array<string> = [
+    "modifierDescription",
+    "modifier",
+    "missionType",
+    "state",
+    "timeLeft",
+    "location",
+    "activation",
+    "active",
+    "expiry",
+    "inventory",
+    "missions",
+    "type",
+    "nightmare",
+    "archwingRequired",
+    "boss",
+    "faction",
+    "credits",
+    "ducats",
+    "eta",
+    "missionType",
+    "item",
+  ];
+  const nestedTitle = Object.keys(nestedTableArray)[0];
+  const nestedArray = nestedTableArray[nestedTitle as any];
+  const { reducedData } = extractRelevantData(nestedArray[0], relevantData);
+  const table = new Table({
+    head: [
+      "",
+      nestedTitle.toUpperCase(),
+      ...(nestedArray.length !== 0
+        ? Object.keys(reducedData).map((name) => name.toUpperCase())
+        : []),
+    ],
+  });
+
+  nestedArray.forEach((value: object, i: number) => {
+    let tmpArr = [];
+    const { reducedData } = extractRelevantData(value, relevantData);
+    console.log(reducedData);
+    for (let j = 0; j < Object.values(reducedData).length; j++) {
+      Object.values(reducedData)[j] !== null
+        ? tmpArr.push(
+            Object.values(reducedData as IWarframeEventStatus)[j].toString()
+          )
+        : tmpArr.push("missing data");
+    }
+    table.push({ [i]: tmpArr });
+  });
+  return table.toString();
+}
+
+function extractRelevantData(
+  data: IWarframeEventStatus,
+  relevantData: Array<string>
+): IWarframeEventStatus {
   let reducedData = {};
   let nestedData: IWarframeEventStatus | null = null;
-  const getKeys = Object.keys(data as object);
+  const getKeys = Object.keys(data);
   if (relevantData.length !== 0) {
     for (let i = 0; i < relevantData.length; i++) {
-      if (relevantData[i] === "expiry" || relevantData[i] === "activation") {
-        const formatTime = format(parseISO(data[relevantData[i]]), "PPpp");
-        reducedData = { [relevantData[i]]: formatTime, ...reducedData };
-      }
+      // if (relevantData[i] === "expiry" || relevantData[i] === "activation") {
+      //   const formatTime = format(parseISO(data[relevantData[i]]), "PPpp");
+      //   reducedData = { [relevantData[i]]: formatTime, ...reducedData };
+      // }
       if (Array.isArray(data[relevantData[i]])) {
         nestedData = { [relevantData[i]]: data[relevantData[i]] };
       }
 
-      // iterates through the rest of the property that is needed
+      // iterate through the remaining properties that are needed
       for (let j = 0; j < getKeys.length; j++) {
         if (
           relevantData[i] === getKeys[j] &&
@@ -135,83 +169,4 @@ function extractRelevantData(data: IWarframeEventStatus): IWarframeEventStatus {
   }
   return { reducedData, nestedData };
 }
-
 export default logCurrentEventStatus;
-
-// ARCHON
-
-// {
-// activation: '2023-08-21T00:00:00.000Z',
-// startString: '-2h 23m 58s',
-// expiry: '2023-08-28T00:00:00.000Z',
-// active: true,
-// rewardPool: 'Archon Sortie Rewards',
-// missions: [
-//   {
-//     node: 'Alator (Mars)',
-//     nodeKey: 'Alator (Mars)',
-//     type: 'Extermination',
-//     typeKey: 'Extermination',
-//     nightmare: false,
-//     archwingRequired: false,
-//     isSharkwing: false,
-//   },
-// ],
-// boss: 'Archon Amar',
-// faction: 'Narmer',
-// expired: false,
-// eta: '6d 21h 36m 1s'
-// }
-
-// Vault trader
-// {
-//   activation: '2023-08-03T18:00:00.000Z',
-//   startString: '-17d 8h 27m 58s',
-//   expiry: '2023-09-07T18:00:00.000Z',
-//   active: true,
-//   location: "Maroo's Bazaar (Mars)",
-//   inventory: [
-//     { item: 'M P V Rhino Prime Single Pack', ducats: 6, credits: null },
-//     {
-//       item: 'M P V Rhino Nyx Prime Dual Pack',
-//       ducats: 10,
-//       credits: null
-//     },
-//     { item: 'M P V Nyx Prime Single Pack', ducats: 6, credits: null },
-//     { item: 'Rhino Prime', ducats: 3, credits: null },
-//     {
-//       item: 'M P V Distilling Extractor Prime Set',
-//       ducats: 1,
-//       credits: null
-//     },
-//     { item: 'Noru Prime Syandana', ducats: 2, credits: null },
-//     { item: 'Ankyros Prime', ducats: 2, credits: null },
-//     { item: 'Boltor Prime', ducats: 2, credits: null },
-//     { item: 'Nyx Prime', ducats: 3, credits: null },
-//     { item: 'M P V Targis Prime Armor Set', ducats: 2, credits: null },
-//     { item: 'Vala Sugatra Prime', ducats: 1, credits: null },
-//     { item: 'Scindo Prime', ducats: 2, credits: null },
-//     { item: 'Hikou Prime', ducats: 2, credits: null },
-//     { item: 'Rhino Prime Bobble Head', ducats: 1, credits: null },
-//     { item: 'Nyx Prime Bobble Head', ducats: 1, credits: null },
-//     {
-//       item: 'T1 Void Projection Rhino Nyx Vault A Bronze',
-//       ducats: null,
-//       credits: 1
-//     },
-//     {
-//       item: 'T2 Void Projection Rhino Nyx Vault A Bronze',
-//       ducats: null,
-//       credits: 1
-//     },
-//     {
-//       item: 'T3 Void Projection Rhino Nyx Vault A Bronze',
-//       ducats: null,
-//       credits: 1
-//     },
-//     {
-//       item: 'T4 Void Projection Rhino Nyx Vault A Bronze',
-//       ducats: null,
-//       credits: 1
-//     }
-//   ]}
